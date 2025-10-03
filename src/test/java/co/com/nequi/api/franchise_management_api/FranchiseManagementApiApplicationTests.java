3738bc96256c0400fde819a9c0d987fa6c6a98fe
Edward Tapiero <etapiero@poligran.edu.co>
2025-10-03 10:15:20 -0500

    Initial commit

---
+++ src/test/java/co/com/nequi/api/franchise_management_api/FranchiseManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.nequi.api.franchise_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
